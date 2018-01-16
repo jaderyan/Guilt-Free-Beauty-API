cd87abcdb602391974d2db68791e9cd3d417012e
jaderyan <jadeelizabethryan@outlook.com>
2018-01-16 20:16:20 +0000

    node_env change

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   DB: {
     test: 'mongodb://localhost/cruelty-free-api-test',
-    dev: 'mongodb://jade:d00ugie@ds055832.mlab.com:55832/cruelty-free',
+    development: 'mongodb://jade:d00ugie@ds055832.mlab.com:55832/cruelty-free',
   },
   PORT: {
     test: 3090,
-    dev: 3000,
+    development: 3000,
   }
 };
